d1e3fa591245afc38077c7331b4b6bae761ec2d4
cocodev <ajorgenmarten35@gmail.com>
2025-09-03 03:15:48 -0400

    raw: harcoded backend url

--- src/components/config.ts
+++ src/components/config.ts
@@ -1,4 +1,4 @@
-export const BACKEND_URL = "http://localhost:3001/";
+export const BACKEND_URL = "https://backoffice-wvr1.onrender.com/";
 export const USERNAME = "admin@admin.com";
 export const PASSWORD = "administrador";
 export const APP_NAME = "Backoffice";
